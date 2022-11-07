7a138b62607122e23843354493476ff6bc28f2be
FFeil <102078772+FFeil@users.noreply.github.com>
2022-11-07 14:50:33 +0100

    ScrollPane hinzugefügt + zoomen mgl

--- src/main/java/de/feil/Controller.java
+++ src/main/java/de/feil/Controller.java
@@ -1,4 +1,37 @@
 package de.feil;
 
+import de.feil.automaton.Automaton;
+import de.feil.automaton.GameOfLifeAutomaton;
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+
 public class Controller {
+
+    @FXML
+    private ScrollPane scrollPane;
+    private PopulationPanel populationPanel;
+
+    private Automaton automaton;
+
+    public void initialize() {
+        automaton = new GameOfLifeAutomaton(60, 60, false);
+        automaton.setState(0, 0, 1);
+        automaton.setState(0, 1, 1);
+        automaton.setState(0, 2, 1);
+
+        populationPanel = new PopulationPanel(automaton);
+        //populationPanel.setStyle("-fx-background-color: blue;");
+
+        scrollPane.setContent(populationPanel);
+    }
+
+    public void OnZoomInAction() {
+        populationPanel.zoomIn();
+        populationPanel.resize(populationPanel.calcCanvasWidth(), populationPanel.calcCanvasHeight());
+    }
+
+    public void OnZoomOutAction() {
+        populationPanel.zoomOut();
+        populationPanel.resize(populationPanel.calcCanvasWidth(), populationPanel.calcCanvasHeight());
+    }
 }
---
+++ src/main/java/de/feil/PopulationPanel.java
@@ -0,0 +1,77 @@
+package de.feil;
+
+import de.feil.automaton.Automaton;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Region;
+import javafx.scene.paint.Color;
+
+public class PopulationPanel extends Region {
+
+    private static double AUTOMATON_WIDTH = 15;
+    private static double AUTOMATON_HEIGHT = 15;
+    private static final double BORDER_WIDTH = 10;
+    private static final double BORDER_HEIGHT = 10;
+
+    private Automaton automaton;
+    private Canvas canvas;
+
+    public PopulationPanel(Automaton automaton) {
+        this.automaton = automaton;
+
+        this.canvas = new Canvas(calcCanvasWidth(), calcCanvasHeight());
+        paintCanvas();
+        this.getChildren().add(canvas);
+    }
+
+
+    private void paintCanvas() {
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        gc.setLineWidth(0.8);
+        gc.setStroke(Color.GRAY);
+
+        for (int i = 0; i < automaton.getNumberOfRows(); i++) {
+            for (int j = 0; j < automaton.getNumberOfColumns(); j++) {
+                if (automaton.getCell(i, j).getState() == 1) {
+                    gc.setFill(Color.BLACK);
+                } else {
+                    gc.setFill(Color.WHITE);
+                }
+                gc.fillRect(BORDER_WIDTH + j * AUTOMATON_WIDTH, BORDER_HEIGHT + i * AUTOMATON_HEIGHT, AUTOMATON_WIDTH,
+                        AUTOMATON_HEIGHT);
+                gc.strokeRect(BORDER_WIDTH + j * AUTOMATON_HEIGHT, BORDER_HEIGHT + i * AUTOMATON_HEIGHT, AUTOMATON_WIDTH,
+                        AUTOMATON_HEIGHT);
+            }
+        }
+    }
+
+    public double calcCanvasWidth() {
+        return 2 * BORDER_WIDTH + AUTOMATON_WIDTH * automaton.getNumberOfColumns();
+    }
+
+    public double calcCanvasHeight() {
+        return 2 * BORDER_HEIGHT + AUTOMATON_HEIGHT * automaton.getNumberOfRows();
+    }
+
+    public void zoomIn() {
+        AUTOMATON_WIDTH++;
+        AUTOMATON_HEIGHT++;
+
+        canvas.setWidth(calcCanvasWidth());
+        canvas.setHeight(calcCanvasHeight());
+        paintCanvas();
+    }
+
+    public void zoomOut() {
+        if (AUTOMATON_HEIGHT > 2 && AUTOMATON_WIDTH > 2) {
+            AUTOMATON_WIDTH--;
+            AUTOMATON_HEIGHT--;
+
+            canvas.setWidth(calcCanvasWidth());
+            canvas.setHeight(calcCanvasHeight());
+            paintCanvas();
+        }
+    }
+}
