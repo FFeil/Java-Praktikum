6819e88832dc9745f76166c61751e18c12d1acef
FFeil <102078772+FFeil@users.noreply.github.com>
2023-01-23 08:37:22 +0100

    SaveSettingsDialog doppelte Namen gefiltert

--- src/main/java/de/feil/view/dialog/SaveSettingsDialog.java
+++ src/main/java/de/feil/view/dialog/SaveSettingsDialog.java
@@ -1,15 +1,17 @@
 package de.feil.view.dialog;
 
+import de.feil.controller.database.DatabaseController;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextInputDialog;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class SaveSettingsDialog extends TextInputDialog {
 
-    public SaveSettingsDialog() {
+    public SaveSettingsDialog(List<String> settings) {
         setTitle("Einstellungen");
         setHeaderText("Unter welchen Namen sollen die Einstellungen\ngespeichert werden? Du darfst höchstens\n40 Zeichen benutzen!");
         setContentText("Name:");
@@ -17,8 +19,10 @@ public class SaveSettingsDialog extends TextInputDialog {
         BooleanBinding only40CharsBindings = Bindings.createBooleanBinding(
                 () -> Pattern.matches("\\S{1,40}",
                         getEditor().getText()), getEditor().textProperty());
+        BooleanBinding duplicateNameBinding = Bindings.createBooleanBinding(
+                () -> settings.contains(getEditor().getText()), getEditor().textProperty());
 
         getDialogPane().lookupButton(ButtonType.OK).disableProperty()
-                .bind(only40CharsBindings.not());
+                .bind(only40CharsBindings.not().or(duplicateNameBinding));
     }
 }
